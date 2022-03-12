598c9fce9d31afd60d55ad9f1d72cf5931112a01
Student USO VM User <miruna1509aldica@gmail.com>
2022-03-12 11:13:43 +0200

    Am facut exercitiile

--- laborator/content/reprezentare-numere/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/4-rotations/rotations.c
@@ -2,22 +2,34 @@
 
 void rotate_left(int *number, int bits)
 {
-	/* TODO */
-	(void) number;
-	(void) bits;
+int aux = *number;
+int i = 1;
+while(i <= bits) {
+*number = (*number <<1);
+i++;
+}
+*number = *number + (aux >>31);
+printf("%d\n", *number);
 }
 
 void rotate_right(int *number, int bits)
 {
-	/* TODO */
-	(void) number;
-	(void) bits;
+int i = 1;
+while(i <= bits) {
+*number = (*number >>1) + (*number <<31);
+i++;
+}
+printf("%d\n", *number);
 }
 
 int main()
 {
-	/* TODO: Test functions */
+int a;
+scanf("%d", &a);
+rotate_right(&a, 1);
+rotate_left(&a, 2);
 
-	return 0;
+return 0;
 }
+
 
