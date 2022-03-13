2c7b4ceac822167224a6c6b9dcbbbcc1a6236ebb
Student USO VM User <miruna1509aldica@gmail.com>
2022-03-13 13:31:52 +0200

    Rezolvarea problemei bonus

--- laborator/content/operatii-memorie-gdb/7-pointers/pointers.c
+++ laborator/content/operatii-memorie-gdb/7-pointers/pointers.c
@@ -3,11 +3,38 @@
 #include <assert.h>
 #include <stdlib.h>
 
-#define SIGN(X) (((X) > 0) - ((X) < 0))
+int my_strcmp(const char *s1, const char *s2) {
+	while(*s1 == *s2 && *s2) {
+		s1++;
+		s2++;
+	}
+	return (int)(unsigned char)(*s1) - (int)(unsigned char)(*s2);
+}
+void *my_memcpy(void *dest, const void *src, size_t n) {
+	unsigned char *destination = (unsigned char *)dest;
+	const char *source = (const char *)src;
+	int index = 0;
+	for(index = 0; index < n; index++) {
+		*(destination++) = *(source++);
+	}
+	return dest;
 
-int my_strcmp(const char *s1, const char *s2);
-void *my_memcpy(void *dest, const void *src, size_t n);
-char *my_strcpy(char *dest, const char *src);
+}
+char *my_strcpy(char *dest, const char *src) {
+	if(dest == NULL)
+		return NULL;
+	char *copy = dest;
+	while(*src != '\0') {
+		*dest = *src;
+		dest ++;
+		src ++;
+	}
+	*dest = '\0';
+	return copy;
+
+
+
+}
 
 int main() {
 	char s1[] = "Abracadabra";
@@ -15,18 +42,15 @@ int main() {
 	char src[] = "Learn IOCLA, you must!";
 	char *dest = malloc(sizeof(src));
 
-	(void) s1;
-	(void) s2;
-
-	/*
-	Decomentati pe rand cate un assert pe masura ce implementati o functie.
-	Daca functia voastra este implementata corect atunci asertia se va realiza
-	cu succes. In caz contrar, programul va crapa.
-	*/
-	// assert(SIGN(my_strcmp(s1, s2)) == SIGN(strcmp(s1, s2)));
-	// assert(my_memcpy(dest, src, sizeof(src)) == memcpy(dest, src, sizeof(src)));
-	// assert(my_strcpy(dest, src) == strcpy(dest, src));
+/*
+Decomentati pe rand cate un assert pe masura ce implementati o functie.
+Daca functia voastra este implementata corect atunci asertia se va realiza
+cu succes. In caz contrar, programul va crapa.
+*/
+	assert(my_strcmp(s1, s2) == strcmp(s1, s2));
+	assert(my_memcpy(dest, src, sizeof(src)) == memcpy(dest, src, sizeof(src)));
+	assert(my_strcpy(dest, src) == strcpy(dest, src));
 	free(dest);
 
-	return 0;
+return 0;
 }
