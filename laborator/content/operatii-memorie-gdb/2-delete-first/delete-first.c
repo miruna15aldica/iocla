7ba44f7a69678243ba10bdb4f85bae8c69bd991f
Student USO VM User <miruna1509aldica@gmail.com>
2022-03-13 13:24:59 +0200

    Rezolvari lab 2

--- laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
+++ laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
@@ -2,17 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* delete_first(char *s, char *pattern);
+char* delete_first(char *s, char *pattern) {
+	int length = strlen(pattern);
+	int found = strlen(s);
+	char *lookfor = strstr(s, pattern);
+	if(lookfor) {
+		int before_s = (lookfor) - (s);
+		char *new_word = malloc( found +1 - length);
+		strncpy(new_word, s, before_s);
+		strcpy(new_word + before_s, lookfor + length);
+		return new_word;
+	}
+	else {
+		return 0;
+	}
+}
 
 int main(){
 	char *s = "Ana are mere";
 	char *pattern = "re";
 
-	(void) s;
-	(void) pattern;
-
-	// Decomentati linia dupa ce ati implementat functia delete_first.
-	// printf("%s\n", delete_first(s, pattern));
+	printf("%s\n", delete_first(s, pattern));
 
 	return 0;
 }
