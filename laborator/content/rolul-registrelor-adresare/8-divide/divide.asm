d7aed955740997cedde8cb2ff1bc4409f2799e11
Student USO VM User <miruna1509aldica@gmail.com>
2022-04-14 14:04:04 +0300

    Lab 6: Ex 8-divide

--- laborator/content/rolul-registrelor-adresare/8-divide/divide.asm
+++ laborator/content/rolul-registrelor-adresare/8-divide/divide.asm
@@ -34,8 +34,34 @@ main:
     PRINTF32 `%s\x0`, string_remainder
     PRINTF32 `%hhu\n\x0`, ebx
 
+    mov edx, [dividend2]
+    mov al, blshr edx, 16
+    mov bl, word [divisor2]
+    div bl
 
+
+    PRINTF32 `%s\x0`, string_quotient
+    xor ebx, ebx
+    mov bl, al
+    PRINTF32 `%hhu\n\x0`, ebx
+    xor ebx, ebx
+    mov bl, ah
+    PRINTF32 `%s\x0`, string_remainder
+    PRINTF32 `%hhu\n\x0`, ebx
+
+    mov eax, dword [dividend3]
+    mov ebx, dword [dividend3+4]
+    mov ecx, dword[divisor3]
+    div ecx
     ; TODO: Calculate quotient and remainder for 67254 / 1349.
+    PRINTF32 `%s\x0`, string_quotient
+    xor ebx, ebx
+    mov bl, al
+    PRINTF32 `%hhu\n\x0`, ebx
+    xor ebx, ebx
+    mov bl, ah
+    PRINTF32 `%s\x0`, string_remainder
+    PRINTF32 `%hhu\n\x0`, ebx
 
     ; TODO: Calculate quotient and remainder for 69094148 / 87621.
     
