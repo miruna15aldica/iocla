bade2494e89baeaa8e895c3616523308d976fda0
Student USO VM User <miruna1509aldica@gmail.com>
2022-04-14 14:03:35 +0300

    Lab 6: Ex 5-6-7-sum array

--- laborator/content/rolul-registrelor-adresare/5-6-7-sum-array/sum_array.asm
+++ laborator/content/rolul-registrelor-adresare/5-6-7-sum-array/sum_array.asm
@@ -29,6 +29,29 @@ add_byte_array_element:
 
 
     ; TODO: Compute sum for elements in word_array and dword_array.
+ mov ecx, ARRAY_SIZE
+    xor eax, eax
+    xor edx, edx
+    
+add_word_array_element:
+    mov dx, word [word_array + 2 * ecx - 2]
+    add eax, edx
+    loop add_word_array_element ; Decrement ecx, if not zero, add another element.
 
+    PRINTF32 `%s\x0`, print_format
+    PRINTF32 `%u\n\x0`, eax
+
+
+    mov ecx, ARRAY_SIZE
+    xor eax, eax
+    xor edx, edx
+
+add_dword_array_element:
+    mov edx, dword [dword_array + 4 * ecx - 4]
+    add eax, edx
+    loop add_dword_array_element ; Decrement ecx, if not zero, add another element.
+
+    PRINTF32 `%s\x0`, print_format
+    PRINTF32 `%u\n\x0`, eax
     leave
     ret
