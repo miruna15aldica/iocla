45b84244e213eb09484105e4ccc486f433ce7443
Student USO VM User <miruna1509aldica@gmail.com>
2022-05-19 15:16:02 +0300

    3 Depanare stack frame

---
+++ laborator/content/interactiune-c-assembly/print-hello.asm
@@ -0,0 +1,22 @@
+extern printf
+
+section .data
+	message db "Hello", 0
+
+section .text
+
+global print_hello
+
+
+
+;   TODO: Adăugați instrucțiunea lipsă
+print_hello:
+	push ebp
+	mov ebp, esp
+
+	push message
+	call printf
+	add esp, 4
+
+	leave
+	ret
